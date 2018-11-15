d88fb3a802b7e072a0b398041aabac23491a9a95
abdiasriver <abdias@alumnos.uaslp.edu.mx>
2018-11-15 01:52:15 -0600

    blog

---
+++ cie/indexc0d0.php
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Inicio</title>
+		<description><![CDATA[]]></description>
+		<link>http://uimqroo.edu.mx/cie/index.php/component/content/?view=featured</link>
+		<lastBuildDate>Thu, 15 Nov 2018 07:40:45 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="http://uimqroo.edu.mx/cie/index.php?format=feed&amp;type=rss"/>
+		<language>es-es</language>
+		<item>
+			<title>Acreditación en el EC0076</title>
+			<link>http://uimqroo.edu.mx/cie/index.php/10-noticias/43-acreditacion-en-el-ec0076</link>
+			<guid isPermaLink="true">http://uimqroo.edu.mx/cie/index.php/10-noticias/43-acreditacion-en-el-ec0076</guid>
+			<description><![CDATA[<div class="feed-description"><h2> NUEVO ESTÁNDAR ACREDITADO: EC0076 EVALUACIÓN DE LA COMPETENCIA DE CANDIDATOS CON BASE EN ESTÁNDARES DE COMPETENCIA </h2>
+
+<embed src="http://www.uimqroo.edu.mx/cie/images/documentos/Cedula de acreditacion EC0076.pdf" width="550" height="500"/>
+</div>]]></description>
+			<author>miguel.leon@uimqroo.edu.mx (Super User)</author>
+			<category>Resaltado</category>
+			<category>Noticias</category>
+			<pubDate>Thu, 22 Sep 2016 10:00:21 +0000</pubDate>
+		</item>
+		<item>
+			<title>Acreditación en el EC0301</title>
+			<link>http://uimqroo.edu.mx/cie/index.php/10-noticias/42-acreditacion-en-el-ec0301</link>
+			<guid isPermaLink="true">http://uimqroo.edu.mx/cie/index.php/10-noticias/42-acreditacion-en-el-ec0301</guid>
+			<description><![CDATA[<div class="feed-description"><h2> NUEVO ESTÁNDAR ACREDITADO: EC0301 DISEÑO DE CURSOS DE FORMACIÓN DEL CAPITAL HUMANO DE MANERA PRESENCIAL GRUPAL, SUS INSTRUMENTOS DE EVALUACIÓN Y MANUALES DEL CURSO </h2>
+
+<embed src="http://www.uimqroo.edu.mx/cie/images/documentos/Cedula de acreditacion EC0301.pdf" width="550" height="500"/>
+</div>]]></description>
+			<author>miguel.leon@uimqroo.edu.mx (Super User)</author>
+			<category>Resaltado</category>
+			<category>Noticias</category>
+			<pubDate>Thu, 22 Sep 2016 09:54:10 +0000</pubDate>
+		</item>
+		<item>
+			<title>Red de extensionistas Universitarios</title>
+			<link>http://uimqroo.edu.mx/cie/index.php/10-noticias/41-red-de-extensionistas-universitarios</link>
+			<guid isPermaLink="true">http://uimqroo.edu.mx/cie/index.php/10-noticias/41-red-de-extensionistas-universitarios</guid>
+			<description><![CDATA[<div class="feed-description"><h2> Convocatoria regional para formar parte de la red de extensionistas universitarios (GEIT-UNIVERSITARIOS) </h2>
+
+<embed src="http://www.uimqroo.edu.mx/cie/images/documentos/Convocatoria red de extensionistas universitarios.pdf" width="550" height="500"/>
+</div>]]></description>
+			<author>miguel.leon@uimqroo.edu.mx (Super User)</author>
+			<category>Resaltado</category>
+			<category>Noticias</category>
+			<pubDate>Thu, 22 Sep 2016 09:31:18 +0000</pubDate>
+		</item>
+		<item>
+			<title>Datos bancarios para pago de alineaciones y evaluaciones ECE</title>
+			<link>http://uimqroo.edu.mx/cie/index.php/10-noticias/40-datos-bancarios-para-pago-de-alineaciones-y-evaluaciones-ece</link>
+			<guid isPermaLink="true">http://uimqroo.edu.mx/cie/index.php/10-noticias/40-datos-bancarios-para-pago-de-alineaciones-y-evaluaciones-ece</guid>
+			<description><![CDATA[<div class="feed-description"><h2> Datos bancarios para pago de alineaciones y evaluaciones ECE</h2>
+
+<embed src="http://www.uimqroo.edu.mx/cie/images/documentos/Datos Bancarios ECE.pdf" width="550" height="500"/>
+</div>]]></description>
+			<author>cyber-kroniqdeli@hotmail.com (administrator)</author>
+			<category>Resaltado</category>
+			<category>Noticias</category>
+			<pubDate>Mon, 28 Mar 2016 09:56:07 +0000</pubDate>
+		</item>
+		<item>
+			<title>Logros UIMQROO</title>
+			<link>http://uimqroo.edu.mx/cie/index.php/10-noticias/25-entidad-de-certificacion</link>
+			<guid isPermaLink="true">http://uimqroo.edu.mx/cie/index.php/10-noticias/25-entidad-de-certificacion</guid>
+			<description><![CDATA[<div class="feed-description"><h2> Logros </h2>
+
+<h3> Acreditación de la Universidad Intercultural Maya de Quintana Roo como Entidad de Certificación y Evaluación de Competencias </h3>
+
+<embed src="http://www.uimqroo.edu.mx/cie/images/documentos/Acreditacin UIMQROO.pdf" width="550" height="500"/>
+
+</div>]]></description>
+			<author>miguel.leon@uimqroo.edu.mx (Super User)</author>
+			<category>Resaltado</category>
+			<category>Noticias</category>
+			<pubDate>Thu, 21 Nov 2013 12:16:00 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
